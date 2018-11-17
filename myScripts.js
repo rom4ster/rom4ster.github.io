4152a2bc54f53af8afacca934d2d5cd960673c33
rom4ster <rohansampat97@gmail.com>
2018-11-17 17:55:22 -0600

    AutoGenerated Commit13169

--- myScripts.js
+++ myScripts.js
@@ -1,1 +1,40 @@
-function autoGen() {}
+$.fn.exists = function () {
+    return this.length !== 0;
+}
+//while(document.readyState != "interactive") console.log("waitingForReady");
+
+var BackgroundArray = [];
+
+
+$(document).ready(function () {
+                  BackgroundArray["#Home"] = "url('spacephoto.jpeg')";
+                  BackgroundArray["#About"] = "url(ripple.jpg)";
+                  BackgroundArray["#Skills"] = "url(keyboardBG2.jpg)";
+                  BackgroundArray["#Projects"] = "url(computer.jpeg)";
+                  BackgroundArray["#Contact"] = "none";
+           
+
+
+
+var text = '#'+$('.active').text();
+$('.bodyDiv').hide();
+//console.log(text, $(text));
+$(text).show();
+//console.log($('.topnav').exists());
+$('.topnav li').click(function(){
+                      if ($(this).hasClass('active')) {
+                      return;
+                      }
+                      $('.topnav li').removeClass('active')
+                      $(this).addClass('active');
+                      var text = '#'+$('.active').text();
+                      $('.bodyDiv').hide();
+                      //console.log(text, $(text));
+                      $(text).show();
+                      $("body").css("background-image", BackgroundArray[text]);
+                      text = '';
+                      
+    });
+});
+
+
